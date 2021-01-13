10e53381b34976b77d0b615d6597c8bcdf2e620a
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2021-01-13 19:36:57 +0530

    update product file

--- routers/products.js
+++ routers/products.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 router.get(`/`,async (req,res)=>{
-    const productList = await Product.find().select('name')
+    const productList = await Product.find().populate('Category');
 
     if(!productList){
         res.status(500).json({success:false})
@@ -14,9 +14,9 @@ router.get(`/`,async (req,res)=>{
  
 
 })
-
+//populate - 
 router.get(`/:id`,async (req,res)=>{
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id).populate('Category');
 
     if(!product){
         res.status(500).json({success:false})
@@ -52,5 +52,36 @@ router.post(`/`, async (req,res)=>{
 
        res.send(product);
 })
+
+router.put(`/:id`,async (req,res)=>{
+
+    const category =await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+     
+
+     const product = Product.findByIdAndUpdate(
+         req.params.id,
+         {
+            name:req.body.name,
+            description:req.body.description,
+            richDescription:req.body.richDescription,
+            image:req.body.image,
+            brand:req.body.brand,
+            price:req.body.price,
+            category:req.body.category,
+            countInStock:req.body.countInStock,
+            rating:req.body.rating,
+            numReviews:req.body.numReviews,
+            isFeatured:req.body.isFeatured,
+         },
+         {new:true}
+     )
+
+     if(!product)
+     return res.status(500).send('the product cannot be created')
+
+     res.send(product);
+})
+
 
 module.exports= router;
