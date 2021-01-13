ecb1e245b82488aaab10d6b24de34297d7c6f439
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2021-01-13 19:08:40 +0530

    update product file

--- routers/products.js
+++ routers/products.js
@@ -3,8 +3,9 @@ const express = require('express');
 const { Category } = require('../models/category');
 const router = express.Router();
 
+
 router.get(`/`,async (req,res)=>{
-    const productList = await Product.find();
+    const productList = await Product.find().select('name')
 
     if(!productList){
         res.status(500).json({success:false})
@@ -14,13 +15,24 @@ router.get(`/`,async (req,res)=>{
 
 })
 
-router.post(`/`,(req,res)=>{
-   
-    const category =Category.findById(req.body.category);
-    if(!category) return res.status(400).send('Invalid Category')
-  
+router.get(`/:id`,async (req,res)=>{
+    const product = await Product.findById(req.params.id);
 
-  const product = new Product({
+    if(!product){
+        res.status(500).json({success:false})
+    }
+    res.send(product);
+ 
+
+})
+
+router.post(`/`, async (req,res)=>{
+   
+    const category =await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+     
+   
+  let product = new Product({
       name:req.body.name,
       description:req.body.description,
       richDescription:req.body.richDescription,
@@ -31,12 +43,12 @@ router.post(`/`,(req,res)=>{
       countInStock:req.body.countInStock,
       rating:req.body.rating,
       numReviews:req.body.numReviews,
-      isFeatured:req.body.isFeatured
+      isFeatured:req.body.isFeatured,
   })
-       product =await product.save();
+       product = await product.save();
 
        if(!product)
-       return res.status(500).send('THE product cannot be created')
+          return res.status(500).send('THE product cannot be created')
 
        res.send(product);
 })
